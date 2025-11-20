fbc507c4c3733616f4caecd2d585f64cdadece8d
Sofia Silvestri <s.silvestri@eldes.it>
2025-11-20 11:56:43 +0000

    Using new method to load library

--- kappa_coder/src/main.rs
+++ kappa_coder/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use libloading::{Library, Symbol};
 use data_model::modules::ModuleStruct;
 use processor_engine::stream_processor::StreamProcessor;
-use processor_engine::ffi::{TraitObjectRepr, import_stream_processor, c_char_to_string};
+use processor_engine::ffi::{TraitObjectRepr, import_stream_processor, c_char_to_string, ModuleHandle};
 
 fn print_usage() {
     println!("Usage: kappa_coder [help|[port=port_number] [addr=server_address]]");
@@ -30,16 +30,11 @@ fn main() {
             });
         }
     }
-
-    let lib = unsafe { Library::new("../../KappaLibrary/target/debug/libdigital_transform.so").unwrap() };
-    let module_handle: Symbol<*mut ModuleStruct> = unsafe { lib.get(b"MODULE\0").unwrap() };
-    let get_processor_modules: Symbol<unsafe extern "C" fn(*const u8, usize, *const u8, usize) -> TraitObjectRepr> =
-        unsafe { lib.get(b"get_processor_modules").unwrap() };
-    let a: ModuleStruct = unsafe { **module_handle };
-
-    println!("Loaded module: {}", c_char_to_string(a.name).unwrap());
+    let handle = ModuleHandle::new("../../KappaLibrary/target/debug/libdigital_transform.so").unwrap();
+    
+    println!("Loaded module: {}", c_char_to_string(handle.module.name).unwrap());
     unsafe {
-        let ptr_proc: TraitObjectRepr = get_processor_modules(b"fft_f32".as_ptr(), 7, b"fft_block".as_ptr(), 9);
+        let ptr_proc: TraitObjectRepr = (handle.get_processor_modules)(b"fft_f32".as_ptr(), 7, b"fft_block".as_ptr(), 9);
         if ptr_proc.vtable.is_null() {
             println!("Processor {} not found", "fft_f32");
             return;
